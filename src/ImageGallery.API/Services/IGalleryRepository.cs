f020e8ba43e96f42fc7c9fbb2aa9ec15ed4a6ffa
Kevin Dockx <kevin.dockx@gmail.com>
2017-09-18 14:48:19 +0200

    Starter solution

---
+++ src/ImageGallery.API/Services/IGalleryRepository.cs
@@ -0,0 +1,17 @@
+﻿using ImageGallery.API.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace ImageGallery.API.Services
+{
+    public interface IGalleryRepository
+    {
+        IEnumerable<Image> GetImages();
+        Image GetImage(Guid id);
+        bool ImageExists(Guid id);
+        void AddImage(Image image);
+        void UpdateImage(Image image);
+        void DeleteImage(Image image);
+        bool Save();
+    }
+}
